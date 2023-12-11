21b9379127b096db4602af422a86e33e1ae285d1
AlexAndrew28 <alex.andrew2803@gmail.com>
2023-12-11 12:02:13 +0000

    update to readme to correct file type

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import find_packages, setup
+from os import path
+
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="vectortoolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(where="vectortoolkit"),
     package_dir={"": "vectortoolkit"},
     version="0.1.0",
